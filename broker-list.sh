be17984699af66a394a8d4b8e04a50e212ac0f04
Hardy <hardy.1506@gmail.com>
2014-12-04 14:09:51 -0800

    comments

--- broker-list.sh
+++ broker-list.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# collect container ID(s)
 CONTAINERS=$(docker ps | grep 9092 | awk '{print $1}')
+#collect boker ports and hosts
 BROKERS=$(for CONTAINER in $CONTAINERS; do docker port $CONTAINER 9092 | sed -e "s/0.0.0.0:/$HOST_IP:/g"; done)
+#prints list of broker host:port, replace space with comma
 echo $BROKERS | sed -e 's/ /,/g'
